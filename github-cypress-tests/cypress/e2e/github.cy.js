23ff345d1877c782d0408fac4ab4149dff16d639
nerdforever88 <nerd.forever88@gmail.com>
2025-12-07 00:42:17 -0300

    add novas features

--- github-cypress-tests/cypress/e2e/github.cy.js
+++ github-cypress-tests/cypress/e2e/github.cy.js
@@ -6,7 +6,6 @@ import RepositoryPage from "./pages/RepositoryPage";
 
 const USERNAME = Cypress.env("GITHUB_USERNAME");
 const PASSWORD = Cypress.env("GITHUB_PASSWORD");
-const EXPECTED_USERNAME = Cypress.env("GITHUB_EXPECTED_USERNAME") || Cypress.env("GITHUB_USERNAME");
 const RANDOM_REPO_NAME = `test-repo-${Math.floor(Math.random() * 10000)}`;
 
 describe("Testes de autenticação e repositório do GitHub", () => {
@@ -33,6 +32,7 @@ describe("Testes de autenticação e repositório do GitHub", () => {
     RepositoryPage.assertWaitForNameValidation();
     RepositoryPage.submitCReateRepository();
     RepositoryPage.assertRepositoryCreated('nerdforever88', RANDOM_REPO_NAME);
+    cy.screenshot(`img/${RANDOM_REPO_NAME}`);
   });
 
   it("03 - Deve realizar Logout com sucesso", () => {
